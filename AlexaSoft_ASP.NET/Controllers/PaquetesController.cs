3af2d8abebe8be23b6b3211bf65742371641ab67
sornerito <samueleonardo.escuela@gmail.com>
2024-04-02 12:59:41 -0500

    servicios

---
+++ AlexaSoft_ASP.NET/Controllers/PaquetesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlexaSoft_ASP.NET.Models;
+
+namespace AlexaSoft_ASP.NET.Controllers
+{
+    public class PaquetesController : Controller
+    {
+        private readonly AlexasoftContext _context;
+
+        public PaquetesController(AlexasoftContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Paquetes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Paquetes != null ? 
+                          View(await _context.Paquetes.ToListAsync()) :
+                          Problem("Entity set 'AlexasoftContext.Paquetes'  is null.");
+        }
+
+        // GET: Paquetes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Paquetes == null)
+            {
+                return NotFound();
+            }
+
+            var paquete = await _context.Paquetes
+                .FirstOrDefaultAsync(m => m.IdPaquete == id);
+            if (paquete == null)
+            {
+                return NotFound();
+            }
+
+            return View(paquete);
+        }
+
+        // GET: Paquetes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Paquetes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPaquete,Nombre,Descripcion,Estado")] Paquete paquete)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(paquete);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(paquete);
+        }
+
+        // GET: Paquetes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Paquetes == null)
+            {
+                return NotFound();
+            }
+
+            var paquete = await _context.Paquetes.FindAsync(id);
+            if (paquete == null)
+            {
+                return NotFound();
+            }
+            return View(paquete);
+        }
+
+        // POST: Paquetes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPaquete,Nombre,Descripcion,Estado")] Paquete paquete)
+        {
+            if (id != paquete.IdPaquete)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(paquete);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaqueteExists(paquete.IdPaquete))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(paquete);
+        }
+
+        // GET: Paquetes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Paquetes == null)
+            {
+                return NotFound();
+            }
+
+            var paquete = await _context.Paquetes
+                .FirstOrDefaultAsync(m => m.IdPaquete == id);
+            if (paquete == null)
+            {
+                return NotFound();
+            }
+
+            return View(paquete);
+        }
+
+        // POST: Paquetes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Paquetes == null)
+            {
+                return Problem("Entity set 'AlexasoftContext.Paquetes'  is null.");
+            }
+            var paquete = await _context.Paquetes.FindAsync(id);
+            if (paquete != null)
+            {
+                _context.Paquetes.Remove(paquete);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PaqueteExists(int id)
+        {
+          return (_context.Paquetes?.Any(e => e.IdPaquete == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ AlexaSoft_ASP.NET/Controllers/ServiciosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlexaSoft_ASP.NET.Models;
+
+namespace AlexaSoft_ASP.NET.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        private readonly AlexasoftContext _context;
+
+        public ServiciosController(AlexasoftContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servicios
+        public async Task<IActionResult> Index()
+        {
+              return _context.Servicios != null ? 
+                          View(await _context.Servicios.ToListAsync()) :
+                          Problem("Entity set 'AlexasoftContext.Servicios'  is null.");
+        }
+
+        // GET: Servicios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.IdServicio == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // GET: Servicios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdServicio,Nombre,Descripcion,TiempoMinutos,Estado")] Servicio servicio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(servicio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios.FindAsync(id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+            return View(servicio);
+        }
+
+        // POST: Servicios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdServicio,Nombre,Descripcion,TiempoMinutos,Estado")] Servicio servicio)
+        {
+            if (id != servicio.IdServicio)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servicio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicioExists(servicio.IdServicio))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.IdServicio == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // POST: Servicios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Servicios == null)
+            {
+                return Problem("Entity set 'AlexasoftContext.Servicios'  is null.");
+            }
+            var servicio = await _context.Servicios.FindAsync(id);
+            if (servicio != null)
+            {
+                _context.Servicios.Remove(servicio);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicioExists(int id)
+        {
+          return (_context.Servicios?.Any(e => e.IdServicio == id)).GetValueOrDefault();
+        }
+    }
+}
